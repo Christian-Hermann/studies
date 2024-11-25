eaf7768170abf5e1c79de1e7ea936c653cb97106
Christian-Hermann <christianghermann@gmail.com>
2024-11-25 16:38:14 -0600

    control flow

---
+++ control-flow.js
@@ -0,0 +1,10 @@
+/** CONTROL FLOW
+ * Control flow is the order in which the computer executes statements in a script.
+ * Code is run in order from the first line in the file to the last line, Unless the computer
+ * runs across the (extremely frequent) structures that change the control flow, such
+ * as conditionals and loops. 
+ * 
+ * Control flow means that when you read a script, you must not only read from start to finish
+ * but also look at program structure and how it affects order of execution. 
+ * 
+ */
